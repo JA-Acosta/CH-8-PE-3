02bb271a680cf6cc04a3bfa3552d644209b5dcdf
JAAR <joe.aaron.acosta@outlook.com>
2023-06-05 11:33:30 -0400

    Version 1: Question Prompt

---
+++ main.cpp
@@ -0,0 +1,33 @@
+// JAAR
+// 06/05/2023
+// C++ Primer Plus 6th Ed
+// CH 8 Program Ex 3
+// Version 1
+
+// Updates:
+// +git local/remote
+// +prompt
+
+// Potential Use cases/errors:
+
+// ------------------------------ Prompt---------------------------------------
+// Create a function that will take some string and return an all caps version
+// have the program loop and ask the user for several inputs.
+// ------------------------------ Prompt---------------------------------------
+
+// Headers:
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+// Global Constants and Structures:
+
+// Prototypes:
+
+
+int main()
+{
+
+}
