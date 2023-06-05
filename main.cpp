9b07223d7a3c0586760509dbb2696c077022d029
JAAR <joe.aaron.acosta@outlook.com>
2023-06-05 12:01:37 -0400

    Version 2: Completed

--- main.cpp
+++ main.cpp
@@ -2,11 +2,10 @@
 // 06/05/2023
 // C++ Primer Plus 6th Ed
 // CH 8 Program Ex 3
-// Version 1
+// Version 2
 
 // Updates:
-// +git local/remote
-// +prompt
+// +toupper function
 
 // Potential Use cases/errors:
 
@@ -22,12 +21,40 @@
 
 using namespace std;
 
-// Global Constants and Structures:
-
 // Prototypes:
+void toupper(string& str);
 
 
 int main()
 {
+	cout << "Enter a word or phrase and I will return it all-caps.\n\n";
+	
+	// !Assume Response is either yes or no
+	string response;
+	do
+	{
+		string phrase;
+		cout << "\tEnter a word or phrase: ";
+		getline(cin, phrase);
+		toupper(phrase);
+		cout << phrase << "\n";
+		cout << "Do you want to enter another phrase?: ";
+		cin >> response;
+		toupper(response);
+	} while (response == "YES");
 
+	cout << "\tDONE\n";
 }
+
+
+// takes each letter in a word or phrase and capitalizes it.
+// Param: string & str
+void toupper(string& str)
+{
+	for (int i = 0; i < str.size(); i++)
+	{
+		str.at(i) = toupper(str.at(i));
+	}
+}
+
+// TODO: Create tolower function
